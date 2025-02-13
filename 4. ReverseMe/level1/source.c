155d4edf712a20aa8fce9dc5aef005db97bd3182
Benjamin Gaertner <bgaertne@k2r2p8.42mulhouse.fr>
2025-02-13 13:35:09 +0100

    level 1 tweaks

--- 4. ReverseMe/level1/source.c
+++ 4. ReverseMe/level1/source.c
@@ -6,8 +6,7 @@ int main() {
 
     printf("Please enter key: ");
     
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = 0;
+    scanf("%s", input);
 
     if (strcmp(input, "__stack_check") == 0) {
         printf("Good job.\n");
