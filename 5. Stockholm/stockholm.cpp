0ca4ba51e222c84ef079e58036f033534893d883
BenJ4368 <benjaming@outlook.fr>
2025-05-29 11:19:56 +0200

    stockholm update

--- 5. Stockholm/stockholm.cpp
+++ 5. Stockholm/stockholm.cpp
@@ -99,7 +99,7 @@ void    handle_options(int argc, char *argv[]) {
     bool silent = false;
     std::vector<std::string> target_extensions = {
         ".der", ".pfx", ".key", ".crt", ".csr", ".p12", ".pem", ".odt",
-        ".ott", ".sxw", ".stw", ".uot.3ds", ".max.3dm", ".ods", ".ots",
+        ".ott", ".sxw", ".stw", ".uot", ".3ds", ".max", ".3dm", ".ods", ".ots",
         ".sxc", ".stc", ".dif", ".slk", ".wb2", ".odp", ".otp", ".sxd",
         ".std", ".uop", ".odg", ".otg", ".sxm", ".mml", ".lay", ".lay6",
         ".asc", ".sqlite3", ".sqlitedb", ".sql", ".accdb", ".mdb", ".db",
@@ -108,27 +108,27 @@ void    handle_options(int argc, char *argv[]) {
         ".cmd", ".bat", ".ps1", ".vbs", ".vb", ".pl", ".dip", ".dch", ".sch",
         ".brd", ".jsp", ".php", ".asp", ".rb", ".java", ".jar", ".class",
         ".sh", ".mp3", ".wav", ".swf", ".fla", ".wmv", ".mpg", ".vob",
-        ".mpeg", ".asf", ".avi", ".mov", ".mp4.3gp", ".mkv.3g2", ".flv",
-        ".jpg", ".jpeg", ".vcd", ".iso", ".backup", ".zip", ".rar.7z",
+        ".mpeg", ".asf", ".avi", ".mov", ".mp4", ".3gp", ".mkv", ".3g2", ".flv",
+        ".jpg", ".jpeg", ".vcd", ".iso", ".backup", ".zip", ".rar", ".7z",
         ".gz", ".tgz", ".tar", ".bak", ".tbk", ".bz2", ".PAQ", ".ARC",
         ".aes", ".gpg", ".vmx", ".vmdk", ".vdi", ".sldm", ".sldx", ".sti",
-        ".sxi.602", ".hwp", ".snt", ".onetoc2", ".dwg", ".pdf", ".wk1",
-        ".wks.123", ".rtf", ".csv", ".txt", ".vsdx", ".vsd", ".edb", ".eml",
+        ".sxi", ".602", ".hwp", ".snt", ".onetoc2", ".dwg", ".pdf", ".wk1",
+        ".wks", ".123", ".rtf", ".csv", ".txt", ".vsdx", ".vsd", ".edb", ".eml",
         ".msg", ".ost", ".pst", ".potm", ".potx", ".ppam", ".ppsx", ".ppsm",
         ".pps", ".pot", ".pptm", ".pptx", ".ppt", ".xltm", ".xltx", ".xlc",
         ".xlm", ".xlt", ".xlw", ".xlsb", ".xlsm", ".xlsx", ".xls", ".dotx",
         ".dotm", ".dot", ".docm", ".docb", ".docx", ".doc", ".wma", ".mid",
         ".m3u", ".m4u", ".djvu", ".svg", ".ai", ".psd", ".nef", ".tiff",
         ".tif", ".cgm", ".raw", ".gif", ".png", ".bmp", ".jpg", ".jpeg",
-        ".vcd", ".iso", ".backup", ".zip", ".rar.7z", ".gz", ".tgz", ".tar",
+        ".vcd", ".iso", ".backup", ".zip", ".rar", ".7z", ".gz", ".tgz", ".tar",
         ".bak", ".tbk", ".bz2", ".PAQ", ".ARC", ".aes", ".gpg", ".vmx", ".vmdk",
-        ".vdi", ".sldm", ".sldx", ".sti", ".sxi.602", ".hwp", ".snt", ".onetoc2",
-        ".dwg", ".pdf", ".wk1", ".wks.123", ".rtf", ".csv", ".txt", ".vsdx",
+        ".vdi", ".sldm", ".sldx", ".sti", ".sxi", ".602", ".hwp", ".snt", ".onetoc2",
+        ".dwg", ".pdf", ".wk1", ".wks", ".123", ".rtf", ".csv", ".txt", ".vsdx",
         ".vsd", ".edb", ".eml", ".msg", ".ost", ".pst", ".potm", ".potx",
         ".ppam", ".ppsx", ".ppsm", ".pps", ".pot", ".pptm", ".pptx", ".ppt",
         ".xltm", ".xltx", ".xlc", ".xlm", ".xlt", ".xlw", ".xlsb", ".xlsm",
         ".xlsx", ".xls", ".dotx", ".dotm", ".dot", ".docm", ".docb", ".docx",
-        ".doc", };
+        ".doc"};
 
     if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0){
         std::cout << "Usage: ./stockholm [key | -option (key)]" << std::endl;
